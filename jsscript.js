44dfbf3994eb94938ea7b11540d1de2e7bd0585e
orieprosper <61972880+orieprosper@users.noreply.github.com>
2020-03-22 22:36:12 +0100

    Add files via upload

---
+++ jsscript.js
@@ -0,0 +1,14 @@
+var name = "Orie, Chukwudi, Prosper"
+console.log(name)
+
+//array
+var courses = ["HTML", "CSS", "JAVASCRIPT"]
+console.log(courses.length)
+console.log(courses[0])
+console.log(courses[1])
+console.log(courses[2])
+for (let i = 0; i <= 200; i++) {
+    if (i % 2 === 1) {
+        console.log(i)
+    }
+}
